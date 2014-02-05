9aa20da24db1dbb2e1039f5ff023063bb2945267
dstieglitz <dstieglitz@stainlesscode.com>
2014-02-05 07:57:07 -0800

    added OpenNLP libraries and some experimental training sets

---
+++ src/main/java/com/stainlesscode/penpal/ext/OpenNLPAddressParserAdapter.java
@@ -0,0 +1,11 @@
+package com.stainlesscode.penpal.ext;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dstieglitz
+ * Date: 2/4/14
+ * Time: 6:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OpenNLPAddressParserAdapter {
+}
